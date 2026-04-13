a7af8fdb44e7a8973a54358e43dc175ba805498d
nadav409 <nadav409@gmail.com>
2026-04-13 16:27:03 +0300

    after adding check files

--- src/app/AppSetup.java
+++ src/app/AppSetup.java
@@ -8,6 +8,7 @@ import model.*;
 import model.Vector;
 import operations.CosineDistance;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
@@ -15,10 +16,15 @@ public class AppSetup {
 
     public static Provider buildProvider() throws Exception {
 
-        runPythonEmbeddingScript();
+        Path fullPath = Path.of("full_vectors.json");
+        Path pcaPath  = Path.of("pca_vectors.json");
 
-        EmbeddingLoader fullLoader = new JsonEmbeddingLoader(Path.of("full_vectors.json"));
-        EmbeddingLoader pcaLoader  = new JsonEmbeddingLoader(Path.of("pca_vectors.json"));
+        if (!Files.exists(fullPath) || !Files.exists(pcaPath)) {
+            runPythonEmbeddingScript();
+        }
+
+        EmbeddingLoader fullLoader = new JsonEmbeddingLoader(fullPath);
+        EmbeddingLoader pcaLoader  = new JsonEmbeddingLoader(pcaPath);
 
         List<RawEmbedding> rawFull = fullLoader.load();
         List<RawEmbedding> rawPca  = pcaLoader.load();
